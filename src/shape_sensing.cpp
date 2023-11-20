8d91ce5645b253648268004d18d59864380eb045
Vittorio <vittorio.francescon@gmail.com>
2023-11-20 17:46:49 +0000

    written dummy for shape sensing

---
+++ src/shape_sensing.cpp
@@ -0,0 +1,54 @@
+#ifndef SHAPE_SENSING_CPP
+#define SHAPE_SENSING_CPP
+
+#include <ros/ros.h>
+#include <adaptive_ctrl/rl_shape.h>
+#include <sensor_msgs/Image.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+Mat extractImg(const sensor_msgs::Image::ConstPtr& msg){
+    Mat img = Mat(msg->height, msg->width, CV_8UC3, const_cast<uchar*>(&msg->data[0]), msg->step);
+    return img;
+}
+
+// Define a callback function to handle incoming Image messages
+void imageCallback(const sensor_msgs::Image::ConstPtr& msg)
+{
+    extractImg(msg);
+}
+
+int main(int argc, char** argv)
+{
+    // Initialize ROS
+    ros::init(argc, argv, "shape_sensing_node");
+    ros::NodeHandle nh;
+
+    // Create a publisher for the "rl_shape" message
+    ros::Publisher shape_pub = nh.advertise<adaptive_ctrl::rl_shape>("rl_shape_topic", 10);
+
+    // Create a subscriber for the "camera_input" topic
+    ros::Subscriber image_sub = nh.subscribe("camera_input", 10, imageCallback);
+
+    // Set the publishing rate to 1Hz
+    ros::Rate rate(1);
+
+    while (ros::ok())
+    {
+        // Create a new "rl_shape" message
+        adaptive_ctrl::rl_shape msg;
+
+        // TODO: Fill in the message fields with your desired values
+
+        // Publish the message
+        shape_pub.publish(msg);
+
+        // Sleep to maintain the desired publishing rate
+        rate.sleep();
+    }
+
+    return 0;
+}
+
+#endif // SHAPE_SENSING_CPP
