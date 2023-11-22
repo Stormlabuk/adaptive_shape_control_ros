13fcd85e88a2b03664c6d2a452ea1d9675f6ef79
Vittorio <vittorio.francescon@gmail.com>
2023-11-22 11:12:06 +0000

    discretising node now publishes when service is triggered

--- scripts/discretising_node.py
+++ scripts/discretising_node.py
@@ -3,19 +3,19 @@ import rospy
 from adaptive_ctrl.msg import rl_angles
 from adaptive_ctrl.srv import DiscretiseCurve, DiscretiseCurveResponse, DiscretiseCurveRequest
 
+class DiscretisingNode:
+    def __init__(self):
+        self.pub = rospy.Publisher('rl_angles', rl_angles, queue_size=10)
+        rospy.init_node('discretising_node', anonymous=True)
+        rospy.Service('discretise_curve', DiscretiseCurve, self.points_to_angles)
+        print("Discretising node started")
+        rospy.spin()
 
-def points_to_angles(req):
-    print(req)
-    return DiscretiseCurveResponse(req.px) #return the same request for now
-
-def DiscretisingNode():
-    rospy.init_node('discretising_node', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    pub = rospy.Publisher('rl_angles', rl_angles, queue_size=10)
-    rospy.Service('discretise_curve', DiscretiseCurve, points_to_angles)
-    print("Discretising node started")
-    rospy.spin()
-
+    def points_to_angles(self, req):
+        res = DiscretiseCurveResponse(req.px)
+        angles_msg = rl_angles(angles=res.angles, count=req.num_points)
+        self.pub.publish(angles_msg)
+        return res
 
 if __name__ == '__main__':
     try:
--- scripts/sample_trigger.py
+++ scripts/sample_trigger.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 import rospy
+
 import numpy as np
 from adaptive_ctrl.srv import DiscretiseCurve, DiscretiseCurveResponse, DiscretiseCurveRequest
 
 
 def sample_trigger():
     print("Waiting for service")
-    px = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
+    px = np.array([1.0, 2.0, 3.0, 0.0, 0.0])
     py = np.array([0.0, 0.0, 0.0, 0.0, 0.0]) 
     num_points = 5
     rospy.wait_for_service('discretise_curve')
@@ -14,11 +15,10 @@ def sample_trigger():
         print("Service found")
         discretise_curve = rospy.ServiceProxy('discretise_curve', DiscretiseCurve)
         resp1 = discretise_curve(num_points, px, py)
-        return resp1.angles
     except rospy.ServiceException as e:
         print("Service call failed: %s"%e)
 
 
 if __name__ == "__main__":
     angles = sample_trigger()
-    print(angles)
+    
