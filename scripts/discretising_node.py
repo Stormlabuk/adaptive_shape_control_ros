364c24098aeb879b66bb8d25e1de7786479b36f1
Vittorio <vittorio.francescon@gmail.com>
2023-11-22 12:03:05 +0000

    rl discretisation pubs angles now

--- scripts/discretising_node.py
+++ scripts/discretising_node.py
@@ -1,21 +1,52 @@
 #!/usr/bin/env python
 import rospy
+import numpy as np
+import matplotlib.pyplot as plt
 from adaptive_ctrl.msg import rl_angles
 from adaptive_ctrl.srv import DiscretiseCurve, DiscretiseCurveResponse, DiscretiseCurveRequest
 
 class DiscretisingNode:
     def __init__(self):
         self.pub = rospy.Publisher('rl_angles', rl_angles, queue_size=10)
+        self.joint_no = 6 # needs to be ros_param later
         rospy.init_node('discretising_node', anonymous=True)
         rospy.Service('discretise_curve', DiscretiseCurve, self.points_to_angles)
         print("Discretising node started")
         rospy.spin()
 
     def points_to_angles(self, req):
-        res = DiscretiseCurveResponse(req.px)
-        angles_msg = rl_angles(angles=res.angles, count=req.num_points)
+        x = np.array(req.px)
+        y = np.array(req.py)
+        
+        xinter, yinter = self.disc_points(x, y)
+        dx = xinter[+1:]-xinter[:-1]
+        dy = yinter[+1:]-yinter[:-1]
+        
+        # Fixing the code
+        vects = np.array([dx, dy]).T
+        th = np.arctan2(vects[+1:, 0] * vects[:-1, 1] - vects[+1:, 1] * vects[:-1, 0], vects[+1:, 0] * vects[:-1, 0] + vects[+1:, 1] * vects[:-1, 1])
+        angles = th * 180 / np.pi
+        
+        res = DiscretiseCurveResponse(angles)
+        angles_msg = rl_angles(angles=res.angles, count=self.joint_no)
         self.pub.publish(angles_msg)
         return res
+
+    def disc_points(self, x, y):
+        dx, dy = x[+1:]-x[:-1],  y[+1:]-y[:-1]
+        ds = np.array((0, *np.sqrt(dx*dx+dy*dy)))
+        s = np.cumsum(ds)
+        xinter = np.interp(np.linspace(0, s[-1], self.joint_no), s, x)
+        yinter = np.interp(np.linspace(0, s[-1], self.joint_no), s, y)
+        xinter = np.insert(xinter, 0, xinter[0])
+        yinter = np.insert(yinter, 0, 0)
+        
+        # Invert xinter and yinter
+        xinter = np.flip(xinter)
+        yinter = np.flip(yinter)
+        return xinter, yinter
+
+
 
 if __name__ == '__main__':
     try:
--- scripts/sample_trigger.py
+++ scripts/sample_trigger.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python
 import rospy
-
+import csv
 import numpy as np
+import os.path
 from adaptive_ctrl.srv import DiscretiseCurve, DiscretiseCurveResponse, DiscretiseCurveRequest
 
 
+def read_csv():
+    filename = os.path.dirname(__file__) + '/../csv/extractedPoints.csv'
+    with open(filename, newline='') as csvfile:
+        data = list(csv.reader(csvfile))
+        skipped_data = data[2:]  # Skip the first two lines
+        N = 348  # Specify the number of rows to read
+        selected_data = skipped_data[:N]  # Read the next N rows
+    data = np.array(selected_data)
+    x = data[:, 0].astype(float)
+    y = data[:, 1].astype(float)
+    if(len(x) != len(y)):
+        print("Error: x and y are not the same length")
+    else:
+        return x, y, len(x)
+
 def sample_trigger():
     print("Waiting for service")
-    px = np.array([1.0, 2.0, 3.0, 0.0, 0.0])
-    py = np.array([0.0, 0.0, 0.0, 0.0, 0.0]) 
-    num_points = 5
+    
+    px, py, num_points = read_csv()
+    
     rospy.wait_for_service('discretise_curve')
     try:
         print("Service found")
