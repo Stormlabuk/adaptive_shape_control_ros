66f81bb40e778613da87047ce4915de41cc34176
Vittorio <vittorio.francescon@gmail.com>
2024-03-06 16:49:55 +0000

    Wrote out skeleton for controller

---
+++ include/control_loop/control_loop.hpp
@@ -0,0 +1,31 @@
+#include <ros/ros.h>
+#include <ros_coils/magField.h>
+#include <shapeforming_msgs/error.h>
+#include <shapeforming_msgs/rl_angles.h>
+
+#include <eigen3/Eigen/Core>
+
+class ControlNode {
+   private:
+    ros::NodeHandle nh_;
+    ros::Subscriber desAnglesSub_, obvAnglesSub_, baseFieldSub_, errorSub_;
+    ros::Publisher errorPub_;
+
+    ros::Timer calcError_;
+
+    Eigen::Vector3d baseField_;
+    Eigen::Vector3d held_field;
+    Eigen::Vector3d desAngles_, obvAngles_;
+
+    int desCount_, obvCount_;
+    int error_ = 0, error_prev_ = 0, error_dot_ = 0;
+
+   public:
+    ControlNode();
+    void desAnglesCallback(const shapeforming_msgs::rl_angles::ConstPtr& msg);
+    void obvAnglesCallback(const shapeforming_msgs::rl_angles::ConstPtr& msg);
+    void baseFieldCallback(const ros_coils::magField::ConstPtr& msg);
+    void ComputeError(const ros::TimerEvent&);
+};
+
+int main(int argc, char* argv[]);
--- src/control_loop.cpp
+++ src/control_loop.cpp
@@ -1,54 +1,64 @@
-#include <ros/ros.h>
-#include <eigen3/Eigen/Core>
-#include <shapeforming_msgs/rl_angles.h>
-#include <shapeforming_msgs/error.h>
-#include <ros_coils/magField.h>
+#include "control_loop/control_loop.hpp"
 
+ControlNode::ControlNode() {
+    obvAnglesSub_ = nh_.subscribe<shapeforming_msgs::rl_angles>(
+        "obv_angles", 1, &ControlNode::obvAnglesCallback, this);
 
-void desAnglesCallback(const shapeforming_msgs::rl_angles::ConstPtr msg)
-{
-    // Process desired angles
-    std::vector<float> desAngles = msg->angles;
-    // TODO: Add your desired angles processing code here
+    desAnglesSub_ = nh_.subscribe<shapeforming_msgs::rl_angles>(
+        "des_angles", 1, &ControlNode::desAnglesCallback, this);
+    
+    baseFieldSub_ = nh_.subscribe<ros_coils::magField>(
+        "base_field", 1, &ControlNode::baseFieldCallback, this);
+
+    errorPub_ = nh_.advertise<shapeforming_msgs::error>("error", 1);
+
+    calcError_ = nh_.createTimer(ros::Duration(0.5), &ControlNode::ComputeError, this);
 }
 
-void obvAnglesCallback(const shapeforming_msgs::rl_angles::ConstPtr& msg)
-{
-    // Process observed angles
-    std::vector<float> obvAngles = msg->angles;
-    // TODO: Add your observed angles processing code here
+void ControlNode::desAnglesCallback(
+    const shapeforming_msgs::rl_angles::ConstPtr& msg) {
+    desAngles_ = Eigen::Vector3d(msg->angles[0], msg->angles[1], msg->angles[2]);
+    desCount_ = msg->count;
 }
 
-void baseFieldCallback(const ros_coils::magField::ConstPtr& msg)
-{
-    // Process base field
-    Eigen::Vector3f baseField = Eigen::Vector3f(msg->bx, msg->by, msg->bz);
-
+void ControlNode::obvAnglesCallback(
+    const shapeforming_msgs::rl_angles::ConstPtr& msg) {
+    obvAngles_ = Eigen::Vector3d(msg->angles[0], msg->angles[1], msg->angles[2]);
+    obvCount_ = msg->count;
 }
 
-int main(int argc, char** argv)
-{
+void ControlNode::baseFieldCallback(
+    const ros_coils::magField::ConstPtr& msg) {
+    baseField_ = Eigen::Vector3d(msg->bx, msg->by, msg->bz);
+}
+
+void ControlNode::ComputeError(const ros::TimerEvent&) {
+    shapeforming_msgs::error error_msg;
+    error_msg.header.stamp = ros::Time::now();
+    
+    if(desCount_ != obvCount_) {
+        ROS_WARN("Desired and observed angles are not synchronized");
+        return;
+    }
+    if(desCount_ == 0 || obvCount_ == 0) {
+        ROS_WARN("Desired or observed angles are not received");
+        return;
+    }
+    Eigen::Vector3d diff = desAngles_ - obvAngles_;
+    error_ = diff.norm();
+    error_dot_ = error_ - error_prev_;
+    error_msg.error = error_;
+    error_msg.error_dot = error_dot_;
+    error_prev_ = error_;
+    errorPub_.publish(error_msg);
+}
+
+
+int main(int argc, char** argv) {
     ros::init(argc, argv, "control_loop_node");
-    ros::NodeHandle nh;
-
-    // Subscribe to desired angles topic
-    ros::Subscriber desAnglesSub = nh.subscribe("/des_angles", 10, desAnglesCallback);
-
-    // Subscribe to observed angles topic
-    ros::Subscriber obvAnglesSub = nh.subscribe("/obv_angles", 10, obvAnglesCallback);
-
-    // Subscribe to base field topic
-    ros::Subscriber baseFieldSub = nh.subscribe("/base_field", 10, baseFieldCallback);
-
-    // Create publishers
-    ros::Publisher fieldPub, errorPub;
-
-    // Create publisher for field topic
-    fieldPub = nh.advertise<ros_coils::magField>("/field", 10);
-    errorPub = nh.advertise<shapeforming_msgs::error>("/error", 10);
-    // TODO: Add your control loop code here
-
-    ros::spin();
-
+    ControlNode controlNode;
+    while(ros::ok()) {
+        ros::spinOnce();
+    }
     return 0;
 }
