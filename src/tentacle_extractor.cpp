ba3a05e104c560ba7340d8b5fabfbdc6743294f9
Vittorio Francescon <vittorio.francescon@gmail.com>
2024-03-29 11:54:03 +0000

    Got centre-line extraction working in cpp

--- src/tentacle_extractor.cpp
+++ src/tentacle_extractor.cpp
@@ -17,6 +17,11 @@ void TentacleExtractor::base_callback(const sensor_msgs::ImageConstPtr& msg) {
         ROS_WARN("Tentacle image is not received");
         return;
     }
+    cv::Mat tent_img = this->tent_img.clone();
+    if (tent_img.channels() != 1) {
+        cv::cvtColor(tent_img, tent_img, cv::COLOR_BGR2GRAY);
+    }
+
     if (msg->data.empty()) {
         ROS_WARN("Base image is not received");
         return;
@@ -34,26 +39,27 @@ void TentacleExtractor::base_callback(const sensor_msgs::ImageConstPtr& msg) {
     cv::Mat tent_only = cv::Mat::zeros(cv_ptr->image.size(), CV_8UC1);
     cv::Mat base_img = cv_ptr->image;
 
-    if (tent_img.channels() != 1) {
-        cv::cvtColor(tent_img, tent_img, cv::COLOR_BGR2GRAY);
-    }
     if (base_img.channels() != 1) {
         cv::cvtColor(base_img, base_img, cv::COLOR_BGR2GRAY);
     }
 
     try {
-        // cv::bitwise_not(base_img, base_img);
+        cv::bitwise_not(base_img, base_img);
         cv::bitwise_xor(base_img, tent_img, tent_only);
+
+        cv::compare(tent_only, 255, tent_only, cv::CMP_EQ);
+
+
         cv::Mat element =
             cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
-        cv::erode(tent_img, tent_img, element);
-        cv::ximgproc::thinning(tent_img, tent_only);
+        cv::erode(tent_only, tent_only, element);
+        cv::ximgproc::thinning(tent_only, tent_only);
     } catch (cv::Exception& e) {
         ROS_ERROR("OpenCV exception: %s", e.what());
         return;
     }
-    cv::imshow(OPENCV_WINDOW, tent_img);
-    cv::waitKey(3);
+    // cv::imshow(OPENCV_WINDOW, tent_img);
+    // cv::waitKey(3);
     return;
 }
 
