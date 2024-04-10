bc7e3587249d0630e146226f76484afb3c1815a7
Vittorio <vittorio.francescon@gmail.com>
2024-04-10 14:42:19 +0100

    Working through high_controller

--- include/high_controller/high_controller.hpp
+++ include/high_controller/high_controller.hpp
@@ -1,10 +1,16 @@
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/Vector3.h>
+#include <heuristic_planners/GetPath.h>
+#include <ros/exception.h>
 #include <ros/ros.h>
+#include <shapeforming_msgs/rl_angles.h>
+#include <std_srvs/SetBool.h>
 
 #include <iostream>
 #include <vector>
 
 /**
- * @todo 
+ * @todo
  * Class will control:
  *      1. Costmap publishing through the /initial_imgproc service
  *      2. Path calculation (listen to insertion_point and insertion_ori)
@@ -12,7 +18,7 @@
  *      4. Get base field for number + 1
  *      6. Apply and insert until number+1 is reached
  *      5. Match sizes of obv and des angles
- *      6. Get controller spinning  
+ *      6. Get controller spinning
  *      7. Repeat when controller is done
  */
 
@@ -20,8 +26,27 @@ class HighController {
    public:
     HighController();
 
+    void desAnglesCallback(const shapeforming_msgs::rl_angles::ConstPtr& msg);
+    void obvAnglesCallback(const shapeforming_msgs::rl_angles::ConstPtr& msg);
+    void insertionOriCallback(const geometry_msgs::Vector3::ConstPtr& msg);
+    void insertionPointCallback(const geometry_msgs::Point::ConstPtr& msg);
+    void goalCallback(const geometry_msgs::Point::ConstPtr& msg);
+
    private:
     ros::NodeHandle nh_;
+    ros::ServiceClient initial_imgproc_, path_client_, precomputation_client_;
+
+    ros::Subscriber insertion_point_sub_, insertion_ori_sub_, goal_sub_;
+    ros::Subscriber des_angles_sub_, obv_angles_sub_;
+    ros::Subscriber path_sub_;
+
+    ros::Publisher des_trunc_, obv_trunc_;
+    ros::Publisher inserter_pub_;
+
+    geometry_msgs::Point goal_ = geometry_msgs::Point(),
+                         insertion_point_ = geometry_msgs::Point();
+
+    shapeforming_msgs::rl_angles des_angles_, obv_angles_;
 };
 
 int main(int argc, char* argv[]);
--- src/high_controller.cpp
+++ src/high_controller.cpp
@@ -1,9 +1,78 @@
 #include "high_controller/high_controller.hpp"
 
+HighController::HighController() {
+    des_angles_sub_ = nh_.subscribe("des_angles", 1,
+                                    &HighController::desAnglesCallback, this);
+    obv_angles_sub_ = nh_.subscribe("obv_angles", 1,
+                                    &HighController::obvAnglesCallback, this);
 
+    insertion_ori_sub_ = nh_.subscribe(
+        "insertion_ori", 1, &HighController::insertionOriCallback, this);
 
+    insertion_point_sub_ = nh_.subscribe(
+        "insertion_point", 1, &HighController::insertionPointCallback, this);
+    goal_sub_ = nh_.subscribe("goal", 1, &HighController::goalCallback, this);
 
+    initial_imgproc_ = nh_.serviceClient<std_srvs::SetBool>("initial_imgproc");
+    initial_imgproc_.waitForExistence();
+    std_srvs::SetBoolRequest boolReq;
+    boolReq.data = true;
+    try {
+        initial_imgproc_.call(boolReq);
+    } catch (const ros::Exception& e) {
+        ROS_ERROR("Failed to call initial_imgproc service, error %s", e.what());
+    }
 
-int main(int argc, char* argv[]){
+    path_client_ = nh_.serviceClient<heuristic_planners::GetPath>("get_path");
+    path_client_.waitForExistence();
+    heuristic_planners::GetPathRequest pathReq;
+    pathReq.start = insertion_point_;
+    pathReq.goal = goal_;
+    try {
+        path_client_.call(pathReq);
+    } catch (const ros::Exception& e) {
+        ROS_ERROR("Failed to call get_path service, error %s", e.what());
+    }
+
+    if (des_angles_.angles.size() != obv_angles_.angles.size()) {
+        ROS_WARN("Desired and observed angles are not the same size");
+        return;
+    }
+}
+
+void HighController::desAnglesCallback(
+    const shapeforming_msgs::rl_angles::ConstPtr& msg) {
+    ROS_INFO("Received desired angles");
+    des_angles_ = *msg;
+}
+
+void HighController::obvAnglesCallback(
+    const shapeforming_msgs::rl_angles::ConstPtr& msg) {
+    ROS_INFO("Received observed angles");
+    obv_angles_ = *msg;
+}
+
+void HighController::insertionOriCallback(
+    const geometry_msgs::Vector3::ConstPtr& msg) {
+    ROS_INFO("Received insertion orientation");
+}
+
+void HighController::insertionPointCallback(
+    const geometry_msgs::Point::ConstPtr& msg) {
+    ROS_INFO("Received insertion point");
+    insertion_point_ = *msg;
+}
+
+void HighController::goalCallback(const geometry_msgs::Point::ConstPtr& msg) {
+    ROS_INFO("Received goal");
+    goal_ = *msg;
+}
+
+int main(int argc, char* argv[]) {
+    ros::init(argc, argv, "high_controller_node");
+    HighController highController;
+    while (ros::ok()) {
+        ros::spinOnce();
+    }
     return 0;
 }
